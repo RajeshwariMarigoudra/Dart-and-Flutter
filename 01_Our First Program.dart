6c492212aac2ec97fe958f05d2071aea713330f9
RajeshwariMarigoudra <78674480+RajeshwariMarigoudra@users.noreply.github.com>
2022-03-29 12:07:09 +0530

    Our First Program

---
+++ 01_Our First Program.dart
@@ -0,0 +1,14 @@
+void main() {
+  //variable declaration and initialization.
+  var name = myName();
+
+  print('My name is $name');
+}
+
+//String -> Type of value that will be returned,
+//myName -> name of function,
+//() -> Argument List
+String myName() {
+  //function body
+  return 'Stephen';
+}
