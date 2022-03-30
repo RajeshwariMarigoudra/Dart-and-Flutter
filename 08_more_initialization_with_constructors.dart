714404627b438e2f38c7bea6e97c2f6a3c2067da
RajeshwariMarigoudra <78674480+RajeshwariMarigoudra@users.noreply.github.com>
2022-03-30 18:40:36 +0530

    More Initialization With Constructors

---
+++ 08_more_initialization_with_constructors.dart
@@ -0,0 +1,14 @@
+//void main() {
+// var deck = new Deck();
+//deck.buildMyCards();
+//}
+class Deck {
+  late List<Card> cards;
+
+  Deck() {}
+}
+
+class Card {
+  late String suit;
+  late String rank;
+}
