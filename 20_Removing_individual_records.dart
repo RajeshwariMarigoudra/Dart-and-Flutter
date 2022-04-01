f645f602e8bd4d71d0b95d7e6035d0b2dcd7cdbc
RajeshwariMarigoudra <78674480+RajeshwariMarigoudra@users.noreply.github.com>
2022-04-01 16:51:00 +0530

    Removing Individual Records

---
+++ 20_Removing_individual_records.dart
@@ -0,0 +1,61 @@
+//
+// removeCard -> Remove a specific card from the deck.
+
+void main() {
+  var deck = new Deck();
+
+  print(deck);
+  print(deck.deal(5));
+  print(deck);
+}
+
+class Deck {
+  List<Card> cards = [];
+
+  Deck() {
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    //double nested for loop
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        //body write here
+        var card = new Card(rank, suit);
+        cards.add(card);
+      }
+    }
+  }
+
+  //constrctor body we can create toString()
+  toString() {
+    return cards.toString();
+  }
+
+  Suffle() {
+    cards.shuffle();
+  }
+
+  cardsWithSuit(String suit) {
+    return cards.where((card) => card.suit == suit);
+  }
+
+  //new method called deal()
+  deal(int handSize) {
+    var hand = cards.sublist(0, handSize);
+    cards = cards.sublist(handSize);
+
+    return hand;
+  }
+}
+
+class Card {
+  late String suit;
+  late String rank;
+
+  // add an constructor
+  Card(this.rank, this.suit);
+
+  toString() {
+    return '$rank of $suit';
+  }
+}
