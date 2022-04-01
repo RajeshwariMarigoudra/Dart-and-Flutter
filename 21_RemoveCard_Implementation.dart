967df49a14c685537c7a170bb52b98aff9df3d21
RajeshwariMarigoudra <78674480+RajeshwariMarigoudra@users.noreply.github.com>
2022-04-01 17:09:51 +0530

    RemoveCard Implementation

---
+++ 21_RemoveCard_Implementation.dart
@@ -0,0 +1,64 @@
+void main() {
+  var deck = new Deck();
+
+  //i'm going call the deck.removeCard()
+  deck.removeCard('Diamonds', 'Ace');
+  print(deck);
+}
+
+class Deck {
+  List<Card> cards = [];
+
+  Object suit;
+
+  Deck() {
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    //double nested for loop
+    for (var suit in suits) {
+      for (var rank in ranks) {
+        //body write here
+        var card = new Card(rank, suit);
+        cards.add(card);
+      }
+    }
+  }
+
+  //constrctor body we can create toString()
+  toString() {
+    return cards.toString();
+  }
+
+  Suffle() {
+    cards.shuffle();
+  }
+
+  cardsWithSuit(String suit) {
+    return cards.where((card) => card.suit == suit);
+  }
+
+  //new method called deal()
+  deal(int handSize) {
+    var hand = cards.sublist(0, handSize);
+    cards = cards.sublist(handSize);
+
+    return hand;
+  }
+
+  removeCard(String suite, String rank) {
+    cards.removeWhere((card) => (card.suit == suit) && (card.rank == rank));
+  }
+}
+
+class Card {
+  late String suit;
+  late String rank;
+
+  // add an constructor
+  Card(this.rank, this.suit);
+
+  toString() {
+    return '$rank of $suit';
+  }
+}
